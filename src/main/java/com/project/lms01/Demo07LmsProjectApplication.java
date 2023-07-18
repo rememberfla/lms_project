978cb791b373b9c8937bb6687d4d6ea65159a505
rememberfla <ecaorez@gmail.com>
2023-07-18 09:55:28 +0900

    start

---
+++ src/main/java/com/project/lms01/Demo07LmsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.project.lms01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo07LmsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo07LmsProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/lms01/Demo07LmsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.lms01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo07LmsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
